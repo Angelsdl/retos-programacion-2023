dbd37e932b463f5c7b7d0b11c0c10bb654787033
Angelsdl <102562058+Angelsdl@users.noreply.github.com>
2023-03-14 16:57:20 +0100

    Reto 0 Añadido - en Python

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/Reto0MoureDev.py
@@ -0,0 +1,12 @@
+def FizzBuzz():
+	for num in range(1,101):
+		if(num%3==0):
+			print("fizz")
+		elif(num%5==0):
+			print("buzz")
+		elif(num%3==0 and num%5==0):
+			print("fizzbuzz")
+		else:
+			print(num)
+
+FizzBuzz()
